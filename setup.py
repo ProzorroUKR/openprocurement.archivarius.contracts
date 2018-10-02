8c7f69851a43c0a054ef00f1a9ca34d03eecd3b4
Dmitriy Belyaev <dmitriy.belyaev@raccoongang.com>
2018-10-02 08:52:58 +0300

    release 1.0.1+dp

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.archivarius.contracts',
-      version='1.0',
+      version='1.0.1+dp',
       description='openprocurement.archivarius.contracts',
       long_description=README,
       classifiers=[
